55245e0ae0e492f52e46e4113714c5dc08d258fb
sangreashim <38829486+sangreashim@users.noreply.github.com>
2018-04-29 18:58:27 +0900

    Update splitByBarcode.py

--- splitByBarcode.py
+++ splitByBarcode.py
@@ -7,10 +7,10 @@ fastq_file      = sys.argv[1]
 barcode_file    = sys.argv[2]
 prefix  = sys.argv[3]
 unmatched       = sys.argv[4]
-path_to_TrimingReads    = sys.argv[5]
+#path_to_TrimingReads    = sys.argv[5]
 barcode_length_dict     = {x.strip().split()[0]:str(len(x.strip().split()[1])) for x in open(barcode_file).readlines()}
 
 os.system('zcat '+fastq_file+ ' | fastx_barcode_splitter.pl --bcfile '+barcode_file+' --suffix ".fq" --prefix '+prefix+'. --bol --exact')
 
 for i in barcode_length_dict.keys():
-        os.system(path_to_TrimingReads+'/TrimmingReads.pl -l '+ barcode_length_dict[i] +' -i '+prefix+'.'+i+'.fq')
+        os.system('TrimmingReads.pl -l '+ barcode_length_dict[i] +' -i '+prefix+'.'+i+'.fq')
